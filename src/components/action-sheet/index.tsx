3f672c1bf16f0ae29c17d89413fc48a1ae3da30a
BianXuerui <407955981@qq.com>
2023-09-15 20:59:58 +0800

    first commit

--- src/components/action-sheet/index.tsx
+++ src/components/action-sheet/index.tsx
@@ -9,15 +9,15 @@ type itemList = {
 interface ItemListProps {
   itemLists: itemList[] | string[];
   keyName?: string;
-  callBack: (item, index) => void;
+  callBack: (item: itemList | string, index: number) => void;
 }
 
 const ActionSheet: FC<ItemListProps> = ({ itemLists, keyName, callBack }) => {
   return (
     <>
       {itemLists.map((item, index) => (
-        <div key={item} onClick={() => callBack?.(item, index)}>
-          <div>{item?.title || item}</div>
+        <div key={index} onClick={() => callBack?.(item, index)}>
+          <div>{typeof item === "object" ? item.title : item}</div>
         </div>
       ))}
     </>
